0ac554edd49ce5922e820eefb8fd18119eb86e56
Djones A. Boni <djboni@gmail.com>
2025-01-03 11:39:50 -0300

    Do not install nerd fonts by default

--- cli.sh
+++ cli.sh
@@ -33,6 +33,6 @@ install_if_absent git vim tmux htop
 (set -x; cd programs && ./neovim.sh)
 (set -x; cd programs && ./ripgrep.sh)
 (set -x; cd programs && ./fd.sh)
-(set -x; cd programs && ./nerdfonts.sh)
+#(set -x; cd programs && ./nerdfonts.sh)
 #(set -x; cd programs && ./zig.sh)
 #(set -x; cd programs && ./node.sh)
