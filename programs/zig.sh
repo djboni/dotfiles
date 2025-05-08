f9e0b0881f7c714270d7111680a3157722f1c293
Djones A. Boni <djboni@gmail.com>
2025-05-08 20:16:23 -0300

    Updage zig to v0.14.0 (fix)

--- programs/zig.sh
+++ programs/zig.sh
@@ -2,7 +2,7 @@
 # Download and install zig.
 set -e
 VERSION=0.14.0
-SHA256=d731dc81e1dff2d5f9b1d1979d554648df6d05f7723d1cc9e37430c7ca88d573
+SHA256=473ec26806133cf4d1918caf1a410f8403a13d979726a9045b421b685031a982
 
 usage() {
 	EXIT_STATUS="$1"
@@ -35,7 +35,7 @@ set -x
 # Get source code
 if [ ! -f "downloads/zig-linux-x86_64-$VERSION.tar.xz" ]; then
 	mkdir -p downloads
-	wget -q "https://ziglang.org/builds/zig-linux-x86_64-$VERSION.tar.xz" -O "downloads/zig-linux-x86_64-$VERSION.tar.xz"
+	wget -q "https://ziglang.org/download/$VERSION/zig-linux-x86_64-$VERSION.tar.xz" -O "downloads/zig-linux-x86_64-$VERSION.tar.xz"
 fi
 
 # Verify the hash
