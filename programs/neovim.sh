8b22346be1dbd58160ad440b7a83328ecab38617
Djones A. Boni <djboni@gmail.com>
2025-07-14 19:41:09 -0300

    Update neovim to v0.11.3

--- programs/neovim.sh
+++ programs/neovim.sh
@@ -12,8 +12,8 @@ else
 	#SHA256=95aaa8e89473f5421114f2787c13ae0ec6e11ebbd1a13a1bd6fcf63420f8073f
 	#LINK="https://github.com/neovim/neovim/releases/download/$VERSION/nvim-linux-x86_64.tar.gz"
 
-	VERSION=v0.11.2
-	SHA256=a9b24157672eb218ff3e33ef3f8c08db26f8931c5c04bdb0e471371dd1dfe63e
+	VERSION=v0.11.3
+	SHA256=02b808a3ee8fc30161e07fe3c3edfb24b28bd0295323ac5dbdd8ec7012cac67d
 	LINK="https://github.com/neovim/neovim/releases/download/$VERSION/nvim-linux-x86_64.tar.gz"
 fi
 
