2c15f7a1e0aefada73c1b71f2691cc53fb5e79e5
Djones A. Boni <djboni@gmail.com>
2025-11-03 20:31:59 -0300

    Updage neovim to v0.11.5

--- programs/neovim.sh
+++ programs/neovim.sh
@@ -8,12 +8,13 @@ if ldd --version | grep -qE 'ldd .* 2\.2[0-8]'; then
 	LINK="https://github.com/neovim/neovim/releases/download/$VERSION/nvim-linux64.tar.gz"
 else
 	# Newer glibc (2.29+)
+
 	#VERSION=v0.10.4
 	#SHA256=95aaa8e89473f5421114f2787c13ae0ec6e11ebbd1a13a1bd6fcf63420f8073f
 	#LINK="https://github.com/neovim/neovim/releases/download/$VERSION/nvim-linux-x86_64.tar.gz"
 
-	VERSION=v0.11.4
-	SHA256=a74740047e73b2b380d63a474282814063d10650cd6cc95efa16d1713c7e616c
+	VERSION=v0.11.5
+	SHA256=b2f91117be5b5ea39edd7297156dc2a4a8df4add6c95a90809a8df19e7ab6f52
 	LINK="https://github.com/neovim/neovim/releases/download/$VERSION/nvim-linux-x86_64.tar.gz"
 fi
 
